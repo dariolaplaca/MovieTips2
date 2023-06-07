e0f7f77ac5ec343f0cd4df827401c516c5965579
Keicabba <peppino_cbr@hotmail.com>
2023-06-07 15:23:57 +0200

    deleted loombok from Account

--- src/main/java/com/gruppo4java11/MovieTips/entities/Account.java
+++ src/main/java/com/gruppo4java11/MovieTips/entities/Account.java
@@ -1,9 +1,6 @@
 package com.gruppo4java11.MovieTips.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
@@ -12,9 +9,7 @@ import java.time.LocalDate;
 //TODO Creare un enumerato per lo record status con (D, A)
 //TODO Vedere filtro globale o query custom per il where
 //TODO Auditable Interface createdBy, createdOn, modifyBy, modifyOn (Account and LocalDate)
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "account")
 public class Account {
@@ -33,5 +28,76 @@ public class Account {
     private LocalDate birthday;
     @Column(nullable = false, name = "user_role")
     private String userRole;
+
+    public Account(long id, String name, String surname, String email, String password, LocalDate birthday, String userRole) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.birthday = birthday;
+        this.userRole = userRole;
+    }
+
+    public Account() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
 }
+
 
